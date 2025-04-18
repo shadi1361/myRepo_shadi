cd74cdfe3de008bb8928a78f916ce01eb158d8de
Shadi-Bagheri <bagheri1983goharshad@gmail.com>
2025-04-18 16:00:22 +0200

    Erstellung Rabatt-API Rabattcode

--- 21_projekt_laden_nextjs/src/app/cart/page.tsx
+++ 21_projekt_laden_nextjs/src/app/cart/page.tsx
@@ -7,9 +7,18 @@ import { formatNumberWithCommas } from "@/utilities/number";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+interface IDiscountData {
+  id: number;
+  code: string;
+  percentage: number;
+}
+
 function Cart() {
   const { cartItems } = useShoppingCartContext();
   const [data, setData] = useState<IProductItemProps[]>([]);
+  const [discountCode, setDiscountCode] = useState("");
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [discountedPrice, setDiscountedPrice] = useState(0);
 
   useEffect(() => {
     axios(`http://localhost:3004/products`).then((result) => {
@@ -17,6 +26,28 @@ function Cart() {
       setData(data);
     });
   }, []);
+
+  // Berechnung des Gesamtpreises vor Anwendung des Rabatts
+  const totalPrice = cartItems.reduce((total, item) => {
+    const selectedProduct = data.find(
+      (product) => product.id == item.id.toString()
+    );
+    return total + (selectedProduct?.price || 0) * item.qty;
+  }, 0);
+
+  // Um auf den Rabattcode zuzugreifen, muss beim Klick des Nutzers auf den Eingabe-Button eine Anfrage an das Backend gesendet werden:
+  const handleSubmitDiscount = () => {
+    axios(`http://localhost:3004/discounts?code=${discountCode}`).then(
+      (result) => {
+        const data = result.data as IDiscountData[];
+
+        const discountedPrice = (totalPrice * data[0].percentage) / 100; // Berechnung des Rabatts
+        const finalPrice = totalPrice - discountedPrice; // Endpreis
+        setFinalPrice(finalPrice);
+        setDiscountedPrice(discountedPrice);
+      }
+    );
+  };
 
   return (
     <Container>
@@ -30,32 +61,27 @@ function Cart() {
 
       <div className="border shadow-md p-4 mt-4">
         <h3>
-          قیمت کل :{" "}
-          <span>
-            {formatNumberWithCommas(
-              cartItems.reduce((total, item) => {
-                const selectedProduct = data.find(
-                  (product) => product.id == item.id.toString()
-                );
-
-                return total + (selectedProduct?.price || 0) * item.qty;
-              }, 0)
-            )}
-            $
-          </span>
+          قیمت کل : <span>{formatNumberWithCommas(totalPrice)}$</span>
         </h3>
         <h3>
-          سود شما ازین خرید : <span>45$</span>{" "}
+          سود شما ازین خرید :{" "}
+          <span>{formatNumberWithCommas(discountedPrice)}$</span>{" "}
         </h3>
         <h3>
-          قیمت نهایی : <span>45$</span>{" "}
+          قیمت نهایی : <span>{formatNumberWithCommas(finalPrice)}$</span>{" "}
         </h3>
         <div>
-          <button className="bg-amber-200 px-4 py-1 rounded mr-4">اعمال</button>
+          <button
+            onClick={handleSubmitDiscount}
+            className="bg-amber-200 px-4 py-1 rounded mr-4"
+          >
+            اعمال
+          </button>
           <input
             className="border"
             placeholder="کد تخفیف را وارد کنید"
             type="text"
+            onChange={(e) => setDiscountCode(e.target.value)}
           />
         </div>
       </div>
