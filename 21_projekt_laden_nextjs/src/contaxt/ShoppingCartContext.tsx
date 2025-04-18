f002851ddf185d8d2a9b5d2ea881de7d99e34d78
Shadi-Bagheri <bagheri1983goharshad@gmail.com>
2025-04-18 16:46:11 +0200

    Im Warenkorb speichern  im localStorage, ohne dass Daten verloren gehen

--- 21_projekt_laden_nextjs/src/contaxt/ShoppingCartContext.tsx
+++ 21_projekt_laden_nextjs/src/contaxt/ShoppingCartContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type ShoppingCartContextProviderProps = {
   children: React.ReactNode;
@@ -13,11 +13,11 @@ type CartItems = {
 
 type TShoppingCartContext = {
   cartItems: CartItems[];
-  handleIncreaseProductQty: (id:number)=> void;
-  getProductQty: (id:number)=>number;
+  handleIncreaseProductQty: (id: number) => void;
+  getProductQty: (id: number) => number;
   cartTotalQty: number;
-  handleDecreaseProductQty:(id:number)=>void;
-  handleRemoveProduct:(id:number)=>void;
+  handleDecreaseProductQty: (id: number) => void;
+  handleRemoveProduct: (id: number) => void;
 };
 const ShoppingCartContext = createContext({} as TShoppingCartContext);
 export const useShoppingCartContext = () => {
@@ -30,9 +30,9 @@ export function ShoppingCartContextProvider({
 }: ShoppingCartContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItems[]>([]);
 
-  const cartTotalQty= cartItems.reduce((totalQty,item)=>{
-    return totalQty+item.qty
-  }, 0)
+  const cartTotalQty = cartItems.reduce((totalQty, item) => {
+    return totalQty + item.qty;
+  }, 0);
 
   const getProductQty = (id: number) => {
     return cartItems.find((item) => item.id == id)?.qty || 0;
@@ -40,7 +40,8 @@ export function ShoppingCartContextProvider({
 
   const handleIncreaseProductQty = (id: number) => {
     setCartItems((currentItems) => {
-      const isNotProductExist = currentItems.find((item) => item.id == id) == null;
+      const isNotProductExist =
+        currentItems.find((item) => item.id == id) == null;
 
       if (isNotProductExist) {
         return [...currentItems, { id: id, qty: 1 }]; //then add a new one to the cart.
@@ -50,7 +51,7 @@ export function ShoppingCartContextProvider({
             //if product already exists in the cart:
             return {
               ...item, //hold the previous plus increasing its quantity by 1.
-              qty: item.qty + 1,
+              qty: item.qty + 1
             };
           } else {
             //if product already exists in the cart but not the one i m looking for:
@@ -64,7 +65,7 @@ export function ShoppingCartContextProvider({
   const handleDecreaseProductQty = (id: number) => {
     setCartItems((currentItems) => {
       const isLastOne = currentItems.find((item) => item.id == id)?.qty === 1; // Fixed syntax and added optional chaining
-  
+
       if (isLastOne) {
         return currentItems.filter((item) => item.id != id); // Remove the item if it's the last one
       } else {
@@ -72,7 +73,7 @@ export function ShoppingCartContextProvider({
           if (item.id == id) {
             return {
               ...item,
-              qty: item.qty - 1, // Decrease the quantity
+              qty: item.qty - 1 // Decrease the quantity
             };
           } else {
             return item; // Return other items unchanged
@@ -82,25 +83,46 @@ export function ShoppingCartContextProvider({
     });
   };
 
-const handleRemoveProduct = (id: number) => {
-  setCartItems((currentItems) => {
-    return currentItems.filter((item) => item.id != id); // Remove the item from the cart
-  });
-};
+  const handleRemoveProduct = (id: number) => {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id != id); // Remove the item from the cart
+    });
+  };
 
+
+
+/*   Daten abrufen(Get data or Fetch data):
+  Dies wird nur einmal ausgeführt,und zwar beim Neuladen der Seite oder wenn Benutzer die Seite zum ersten Mal besucht. Falls bereits Daten im localStorage vorhanden sind, werden sie abgerufen, mit JSON.parse in ein Objekt umgewandelt und in die Variable storedCartItems(also den Warenkorb) geladen.
+   */
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
+
+/*
+Daten setzen(Store data Or Set data):
+Immer wenn sich cartItems ändert, wird useEffect erneut ausgeführt
+Dann wird das Item im localStorage gesetzt:
+Die Daten werden mit JSON.stringify in einen String umgewandelt, da localStorage nur Strings speichern kann. 
+Die Daten werden in einen String umgewandelt und unter dem Namen cartItems im localStorage gespeichert,jedes Mal, wenn sich [cartItems] ändert: 
+*/
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]); //Jedes Mal diese sich ändert,führt useEffect() wieder durch.
 
   return (
     <ShoppingCartContext.Provider
-     value={{
-       cartItems,
-       handleIncreaseProductQty,
-       getProductQty,
-       cartTotalQty,
-       handleDecreaseProductQty,
-       handleRemoveProduct,
-       }}
-       >
-
+      value={{
+        cartItems,
+        handleIncreaseProductQty,
+        getProductQty,
+        cartTotalQty,
+        handleDecreaseProductQty,
+        handleRemoveProduct
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
