ae15461a216a6b310e38956336b7956f3680059e
Shadi-Bagheri <bagheri1983goharshad@gmail.com>
2025-05-02 23:31:43 +0200

    Store wurde fertiggestellt

--- 22_projekt_laden_nextjs - Erweitet/src/app/store/page.tsx
+++ 22_projekt_laden_nextjs - Erweitet/src/app/store/page.tsx
@@ -1,10 +1,8 @@
 import Container from "@/components/Container";
 import ProductItem, { IProductItemProps } from "@/components/ProductItem";
-import Search from "@/components/Search";
-import Logo from "@/components/Logo";
 import Link from "next/link";
 import React from "react";
-import NavBottomRight from "@/components/NavBottomRight";
+
 
 async function Store() {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3004";
@@ -13,23 +11,8 @@ async function Store() {
 
   return (
     <Container>
-      <div className="flex flex-col min-h-screen mt-10 relative">
-        {/* Header */}
-        <div className="flex items-center justify-between mb-2 w-full h-20 px-4">
-          <div className="flex-shrink-0">
-            <Logo />
-          </div>
-
-          <div className="flex-grow flex justify-center">
-            <Search />
-          </div>
-
-          <div className="flex-shrink-0">
-            <NavBottomRight />
-          </div>
-        </div>
-        {/* Product Grid */}
-        <div className="grid grid-cols-4 gap-4 py-4 w-full">
+      <div className="flex flex-col min-h-screen mt-0 relative">      
+        <div className="grid grid-cols-4 gap-4 py-2 w-full">
           {data.map((item) => (
             <Link key={item.id} href={`/store/${item.id}`}>
               <ProductItem {...item} />
--- 22_projekt_laden_nextjs - Erweitet/src/components/Layout.tsx
+++ 22_projekt_laden_nextjs - Erweitet/src/components/Layout.tsx
@@ -1,10 +1,13 @@
-/* Mit dem Erstellen der Datei Layout.tsx im components und dem Einbinden dieser Komponente in die Hauptlayout-Datei bzw. layout.tsx wird der children-Inhalt durch die neue Layout.tsx-Komponente geleitet. Dadurch kann die Navbar in die erstellte Layout.tsx-Komponente verschoben werden.Wenn später noch eine Sidebar und ein Footer zum Projekt hinzugefügt werden, bleibt das Hauptlayout der Anwendung übersichtlich und aufgeräumt.
+/* Mit dem Erstellen der Datei Layout.tsx im components und dem Einbinden dieser Komponente in die Hauptlayout-Datei bzw. layout.tsx wird der children-Inhalt durch die neue Layout.tsx-Komponente geleitet. Dadurch kann die Navbar in die erstellte Layout.tsx-Komponente verschoben werden. Wenn später noch eine Sidebar und ein Footer zum Projekt hinzugefügt werden, bleibt das Hauptlayout der Anwendung übersichtlich und aufgeräumt.
  */
 
 import React from "react";
 import Navbar from "./Navbar";
 import TopNavbar from "./TopNavbar";
-
+import Logo from "./Logo";
+import Search from "./Search";
+import NavBottomRight from "./NavBottomRight";
+import Container from "./Container";
 
 interface ILayoutProps {
   children: React.ReactNode;
@@ -16,7 +19,29 @@ function Layout({ children }: ILayoutProps) {
       <TopNavbar />
       <Navbar />
 
-      
+      {/* Flexbox zum Platzieren der Komponenten nebeneinander */}
+
+      <Container>
+        <div className="flex items-center justify-between px-4 py-2 bg-wh
+        ">
+          {/* Logo */}
+
+          <div className="flex-shrink-0">
+            <Logo />
+          </div>
+
+          {/* Suchleiste */}
+          <div className="flex-grow mx-4">
+            <Search />
+          </div>
+
+          {/* NavBottomRight */}
+          <div className="flex-shrink-0">
+            <NavBottomRight />
+          </div>
+        </div>
+      </Container>
+      {/* Seiteninhalt */}
       {children}
     </div>
   );
