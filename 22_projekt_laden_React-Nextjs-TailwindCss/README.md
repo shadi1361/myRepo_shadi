bcecb6568ab358d2deb477ae6709e3852e4db66a
Shadi-Bagheri <bagheri1983goharshad@gmail.com>
2025-05-07 15:35:58 +0200

    Grid Abschnitte und Pagination

---
+++ 22_projekt_laden_React-Nextjs-TailwindCss/README.md
@@ -0,0 +1,8 @@
+Ausführung der Datei :  
+
+1.  npm run dev  
+
+
+2. In Neue Terminal :  
+ json-server --watch src/database/db.json --port 3004    
+ 
--- 22_projekt_laden_React-Nextjs-TailwindCss/next.config.ts
+++ 22_projekt_laden_React-Nextjs-TailwindCss/next.config.ts
@@ -2,11 +2,27 @@
 const nextConfig = {
   images: {
     domains: [
+      "bennysjolind.com",
       "miro.medium.com",
       "imgv3.fotor.com",
       "bestinau.com.au",
       "www.powertrafic.fr",
-      "th.bing.com", // Add this domain
+      "th.bing.com",
+      "cdn.pixabay.com",
+      "www.justrunlah.com",
+      "sneakernews.com",
+      "meta-endurance.com",
+      "sneakerbardetroit.com",
+      "cdna.lystit.com",
+      "i.ytimg.com",
+      "hips.hearstapps.com",
+      "cdn.shopify.com",
+      "i.ytimg.com",
+      "www.sportsdirect.com",
+      "cdn.shopify.com",
+      "staticg.sportskeeda.com",
+      "imboldn.com",
+      "sneakernews.com",
     ],
   },
 };
