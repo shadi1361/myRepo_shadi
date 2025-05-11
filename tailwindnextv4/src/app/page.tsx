7ca670ee6b8c7c0c6ef17e226384458211bbc38f
Shadi-Bagheri <bagheri1983goharshad@gmail.com>
2025-05-11 17:38:08 +0200

    Erstellung Twitter clone

---
+++ tailwindnextv4/src/app/page.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Sidebar from "../../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Twitter-Klon</title>
+        <meta name="description" content="Dies ist ein Twitter-Klon." />
+      </Head>
+
+      {/*sidebar*/}
+      <Sidebar />
+
+      {/*feed*/}
+
+      {/*widgets*/}
+
+      {/*modules*/}
+    </div>
+  );
+}
