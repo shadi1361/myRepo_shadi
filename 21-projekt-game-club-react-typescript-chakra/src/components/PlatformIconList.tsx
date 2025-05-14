1894f36dfe0aed3ddc085ffe0c15340dea675c1c
Shadi-Bagheri <bagheri1983goharshad@gmail.com>
2025-05-14 23:46:27 +0200

    Icons designing

---
+++ 21-projekt-game-club-react-typescript-chakra/src/components/PlatformIconList.tsx
@@ -0,0 +1,40 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid
+} from "react-icons/fa";
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+export default function PlatformIconList({ platforms }: Props) {
+  const iconMap:{[key:string]: IconType } = {
+    pc: FaWindows,
+    playStation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe
+  };
+
+  return (
+    <HStack marginY='13px'>
+      {platforms.map(
+        (platform) => <Icon as={iconMap[platform.slug]} color='gray.500' />)}
+    </HStack>
+  );
+}
--- 21-projekt-game-club-react-typescript-chakra/src/hooks/useGames.ts
+++ 21-projekt-game-club-react-typescript-chakra/src/hooks/useGames.ts
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -20,10 +27,10 @@ const useGames = () => {
     apiClient
       .get<FetchGamesResponse>("/games")
       .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message))
-    }, []);
+      .catch((err) => setError(err.message));
+  }, []);
 
-  return {games,error}
-}
+  return { games, error };
+};
 
 export default useGames;
